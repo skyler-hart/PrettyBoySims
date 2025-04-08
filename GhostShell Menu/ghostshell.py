2569ab4e2019531146166ec40df98acb152ba3af
Skyler Hart <skyler@skylerhart.com>
2025-04-07 22:30:18 -0400

    updated script

--- GhostShell Menu/ghostshell.py
+++ GhostShell Menu/ghostshell.py
@@ -22,13 +22,15 @@ def _remove_traits(sim, output, trait_manager, traits):
 
 def _apply_skills(sim, output, skill_manager, skills):
     for skill_id in skills:
-        skill = skill_manager.get(skill_id)
+        skill = skill_manager.get(get_resource_key(skill_id, Types.STATISTIC))
         if skill:
             tracker = sim.get_tracker(skill)
             if not tracker.has_statistic(skill):
                 tracker.add_statistic(skill)
             tracker.set_value(skill, float(skill.max_level))
             output(f"Maxed skill {skill_id}")
+        else:
+            output(f"Failed to resolve skill {skill_id}")
 
 # Dictionary populated from the spreadsheet logic
 ghostshell_trait_commands = {
@@ -38,7 +40,7 @@ ghostshell_trait_commands = {
         "IfFemaleAdd": [],
         "IfFemaleRemove": [],
         "IfTeenAdd": [3455194433],
-        "skills": [5255876730914093336, 4641759162532272279, 11311871620839888931, 13681797894157322134, 15755652231701198849, 18282882584276959514, 10913272126822913979],
+        "skills": [],
         "IfTeenSkills": [],
         "IfChildSkills": [],
         "IfToddlerSkills": []
@@ -49,7 +51,7 @@ ghostshell_trait_commands = {
         "IfFemaleAdd": [1690154921011321723],
         "IfFemaleRemove": [],
         "IfTeenAdd": [3455194433],
-        "skills": [5255876730914093336, 4641759162532272279, 11311871620839888931, 13681797894157322134, 15755652231701198849, 18282882584276959514, 12503627023949270877, 16649350022740743409],
+        "skills": [],
         "IfTeenSkills": [],
         "IfChildSkills": [],
         "IfToddlerSkills": []
@@ -60,7 +62,7 @@ ghostshell_trait_commands = {
         "IfFemaleAdd": [],
         "IfFemaleRemove": [],
         "IfTeenAdd": [3455194433],
-        "skills": [11311871620839888931, 12503627023949270877, 16649350022740743409],
+        "skills": [],
         "IfTeenSkills": [],
         "IfChildSkills": [],
         "IfToddlerSkills": []
@@ -71,18 +73,18 @@ ghostshell_trait_commands = {
         "IfFemaleAdd": [],
         "IfFemaleRemove": [],
         "IfTeenAdd": [3455194433],
-        "skills": [11311871620839888931, 12503627023949270877, 16649350022740743409],
+        "skills": [],
         "IfTeenSkills": [],
         "IfChildSkills": [],
         "IfToddlerSkills": []
     },
     "service": {
-        "add": [],
+        "add": [35504, 114410, 234887, 234566, 32111],
         "remove": [],
-        "IfFemaleAdd": [1690154921011321723, 13329958653635829193, 17143627558403278375, 4962284592606331639, 4027040388],
+        "IfFemaleAdd": [1690154921011321723, 13329958653635829193, 17143627558403278375, 4962284592606331639, 18084875558065733633, 4027040388],
         "IfFemaleRemove": [],
         "IfTeenAdd": [3455194433],
-        "skills": [11311871620839888931, 16649350022740743409, 231908, 16659, 16700, 16704],
+        "skills": [231908, 16659, 16700, 16704],
         "IfTeenSkills": [],
         "IfChildSkills": [],
         "IfToddlerSkills": []
@@ -93,7 +95,7 @@ ghostshell_trait_commands = {
         "IfFemaleAdd": [],
         "IfFemaleRemove": [],
         "IfTeenAdd": [3455194433],
-        "skills": [11311871620839888931, 12503627023949270877, 16649350022740743409],
+        "skills": [],
         "IfTeenSkills": [],
         "IfChildSkills": [],
         "IfToddlerSkills": []
@@ -104,11 +106,33 @@ ghostshell_trait_commands = {
         "IfFemaleAdd": [],
         "IfFemaleRemove": [],
         "IfTeenAdd": [3455194433],
-        "skills": [11311871620839888931, 12503627023949270877, 16649350022740743409],
+        "skills": [],
         "IfTeenSkills": [],
         "IfChildSkills": [],
         "IfToddlerSkills": []
     },
+    "ww": {
+        "add": [],
+        "remove": [],
+        "IfFemaleAdd": [],
+        "IfFemaleRemove": [],
+        "IfTeenAdd": [],
+        "skills": [5255876730914093336, 4641759162532272279, 11311871620839888931, 18282882584276959514],
+        "IfTeenSkills": [],
+        "IfChildSkills": [],
+        "IfToddlerSkills": []
+    },
+    "sub": {
+        "add": [],
+        "remove": [],
+        "IfFemaleAdd": [],
+        "IfFemaleRemove": [],
+        "IfTeenAdd": [],
+        "skills": [16649350022740743409],
+        "IfTeenSkills": [],
+        "IfChildSkills": [],
+        "IfToddlerSkills": []
+    }
     # Add more command blocks from spreadsheet here as needed
 }
 
